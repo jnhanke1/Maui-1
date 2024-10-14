b32ba84a22eae2d4b0d5fabef1d244fd7a39328a
Jessica Hanke <122949564+jnhanke1@users.noreply.github.com>
2024-10-14 17:33:51 -0500

    1st attempt

--- Maui/obj/Debug/net8.0/Maui.AssemblyInfo.cs
+++ Maui/obj/Debug/net8.0/Maui.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Maui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad85c1804d85eb8e255f4077c8385d36f2b48d15")]
 [assembly: System.Reflection.AssemblyProductAttribute("Maui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
